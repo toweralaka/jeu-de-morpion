a6f5d708adc8f4a9ac2068d518a77199c888fb2c
ola <olaidealaka12@gmail.com>
2025-06-20 14:00:07 +0100

    feat: Add player name input and diagonal wins
    - Add diagonal wins
    - Add inputs for player names and style
    - Refactor functions to handle player names input
    - Show winner on game end factoring ties

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const newGameBoard = (function gameBoard(){
+const gameBoard = (function(){
     let boardSize = 3;
     let board = [];
     let markers = ["X", "O"]
@@ -24,47 +24,63 @@ const newGameBoard = (function gameBoard(){
         }
         return true
     }
-    const checkRowWin = (markers)=>{
+
+    const getRowMatch = ()=>{
+        let rowMatch = []
         for(let i = 0; i < boardSize; i++){
-            let firstItem = board[i][0];
-            if(markers.includes(firstItem) &&
-                (board[i].every(item=>item === firstItem)) == true){
-                winMarker = firstItem;
-                return true
-            }
+            rowMatch.push(board[i])
         }
-        return false
+        return rowMatch
     }
 
-    const checkColumnWin = (markers)=>{
+    const getColumnMatch = ()=>{
         let columnArray = []
         for(let i = 0; i < boardSize; i++){
             columnArray.push(i)
         }
         // Get values in each column
         const columns = columnArray.map(colIndex => board.map(row => row[colIndex]));
+        return columns
+    }
 
+    const getDiagonalMatch = ()=>{
+        const diagonals = [];
+        let rightDiagonal = [];
         for(let i = 0; i < boardSize; i++){
-            let firstItem = columns[i][0];
+            for(let j = 0; j < boardSize; j++){
+                if(i==j){
+                    rightDiagonal.push(board[i][j])
+                }
+            }
+        }
+        diagonals.push(rightDiagonal);
+        let leftDiagonal = [];
+        let iCount = 0;
+        for(let i = 0; i < boardSize; i++){
+            iCount++
+            let jCount = 0;
+            for(let j = boardSize-1; j >= 0; j--){
+                jCount++;
+                if(iCount==jCount){
+                    leftDiagonal.push(board[i][j])
+                }
+            }
+        }
+        diagonals.push(leftDiagonal);
+        return diagonals
+    }
+
+    const isWin = ()=>{
+        let allMatches = [...getColumnMatch(),
+            ...getRowMatch(), ...getDiagonalMatch()];
+        for(let i = 0; i < allMatches.length; i++){
+            let firstItem = allMatches[i][0];
             if(markers.includes(firstItem) &&
-                (columns[i].every(item=>item === firstItem)) == true){
+                (allMatches[i].every(item=>item === firstItem)) == true){
                 winMarker = firstItem;
                 return true
             }
         }
-        return false
-    }
-
-    const isWin = ()=>{
-        // check row win
-        if(checkRowWin(markers)==true){
-            return true
-        }
-        // check column win
-        if(checkColumnWin(markers)==true){
-            return true
-        }
-        // check diagonal win
         return false
     }
 
@@ -95,15 +111,15 @@ const newGameBoard = (function gameBoard(){
         isWin, getWinMarker, getBoardSize, resetBoard}
 })()
 
-function GameController(){
-    let gameBoard = newGameBoard;
-    let markers = newGameBoard.getMarkers();
-    let playerOne = player("Ola1", markers[0]);
-    let playerTwo = player("Ola2", markers[1]);
-    let players = [playerOne, playerTwo];
-    let currentPlayer = players[0];
+const GameController = (function(){
+    // let gameBoard = gameBoard;
+    let markers = gameBoard.getMarkers();
+    let playerOne = null;
+    let playerTwo = null;
+    let players = [];
+    let currentPlayer = null;
     let gameOver = false;
-    let status = "";
+    let status = ""
     let scores = "";
 
     const getScores = ()=>scores;
@@ -116,15 +132,19 @@ function GameController(){
         status = `It is ${currentPlayer.name}'s turn`;
     }
 
+    const setPlayers = (nameOne, nameTwo)=>{
+        playerOne = player(nameOne, markers[0]);
+        playerTwo = player(nameTwo, markers[1]);
+        players.push(playerOne);
+        players.push(playerTwo);
+        currentPlayer = players[0];
+    }
     const getWinner = ()=>{
         if(gameBoard.isWin() == false){ return null}
         return players.filter((player)=>player.marker === gameBoard.getWinMarker())[0]
     }
 
-    const checkGame = ()=>{
-        if(gameBoard.isFilled() == true){
-            gameOver = true;
-        }
+    const checkWinning = ()=>{
         let theWinner = getWinner();
         if(gameBoard.isWin() == true && theWinner != null){
             gameOver = true;
@@ -134,6 +154,15 @@ function GameController(){
             ${playerOne.name}: ${playerOne.getPoints()},
             ${playerTwo.name}: ${playerTwo.getPoints()}`
             currentPlayer = theWinner;
+        }else{
+            if(gameBoard.isFilled() == true){
+                gameOver = true;
+                status = "You draw! Start new round.";
+                scores = `
+                    ${playerOne.name}: ${playerOne.getPoints()},
+                    ${playerTwo.name}: ${playerTwo.getPoints()}`
+                currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+            }
         }
     }
 
@@ -141,6 +170,7 @@ function GameController(){
         gameOver = false;
         currentPlayer = players[0];
         status = `It is ${currentPlayer.name}'s turn`;
+        scores = ""
 
     }
 
@@ -149,12 +179,14 @@ function GameController(){
         if(gameOver == true){return}
         // if marker was dropped succesfully
         if(gameBoard.dropMarker(theRow, theCol, currentPlayer.marker)==true){
-            checkGame()
+            checkWinning()
             switchPlayerTurn()
         }
     }
-    return {playTurn, getScores, getStatus, setRound}
-}
+    return {
+        setPlayers,
+        playTurn, getScores, getStatus, setRound}
+})()
 
 function player(name, marker){
     let points = 0;
@@ -163,18 +195,37 @@ function player(name, marker){
     return {name, marker, getPoints, addPoint}
 }
 
-function domDisplayController(){
+const domDisplayController = (function (){
     let scoresDiv = document.querySelector("#scores-div");
     let statusDiv = document.querySelector("#status-div");
     let boardDiv = document.querySelector("#board-div");
     let startRoundBtn = document.querySelector("#round-btn");
     let restartBtn = document.querySelector("#restart-btn");
+    let startBtn = document.querySelector(".start-btn");
+    let playerDiv = document.querySelector(".players-div");
     // let firstPlayerName
-    let newGame = GameController()
+    let newGame = GameController;
 
-    // const getPlayersName = ()=>{
+    const getPlayersName = ()=>{
+        let nameOne = document.querySelector("input[name='player-one']").value;
+        let nameTwo = document.querySelector("input[name='player-two']").value;
+        if(nameOne==null || nameTwo==null){
+            return false
+        }
+        if(nameOne == nameTwo){
+            return false
+        }
+        newGame.setPlayers(nameOne, nameTwo);
+        return true
+    }
 
-    // }
+    const startGame = ()=>{
+        if(getPlayersName()==true){
+            playerDiv.classList.add("hide");
+            newGame.setRound();
+            renderBoard()
+        }
+    }
     const showScores = ()=>{
         scoresDiv.textContent = newGame.getScores();
     }
@@ -184,7 +235,7 @@ function domDisplayController(){
     }
     const renderBoard = ()=>{
         boardDiv.innerHTML = "";
-        let theBoard = newGameBoard.board
+        let theBoard = gameBoard.board
         for(let i = 0; i < theBoard.length; i++){
             for(let j = 0; j < theBoard.length; j++){
                 let newBtn = document.createElement("button");
@@ -201,14 +252,14 @@ function domDisplayController(){
         showStatus();
     }
     const cleanBoard = ()=>{
-        newGameBoard.resetBoard();
-        renderBoard();
+        gameBoard.resetBoard();
         newGame.setRound();
+        renderBoard();
     }
     startRoundBtn.addEventListener("click", cleanBoard);
     restartBtn.addEventListener("click", ()=>{
-        //reload page
+        window.location.reload()
     })
-    renderBoard()
-}
-domDisplayController()
+    startBtn.addEventListener("click", startGame)
+})()
+domDisplayController;
